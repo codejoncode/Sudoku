829ec92a47b11de4f81392db3984fe6ac0f29779
codejoncode <jonathanjholloway@gmail.com>
2019-01-02 17:47:43 -0600

    cleaned up the code a bit had the back tracker object in the code a couple different times added it to the state and deleted / adjusted the code to use the back tracker on state. clearing out several lines of code. also the while loop that builds the board had  backtracker > 81  which is not correct  backtrackerIndex > 81 is what it should be however it broke the algorithm producing all zeros so I took the second part out and left the first for when the sum equals max input

--- client/src/App.js
+++ client/src/App.js
@@ -19,65 +19,9 @@ class App extends Component {
       "orange",
       "yellow"
     ],
-    colorsIndex: 0,
-    grid: [],
-    board: [],
-    group1: [], 
-    group2: [], 
-    group3: [],
-    group4: [], 
-    group5: [],
-    group6: [], 
-    group7: [],
-    group8: [], 
-    group9: [], 
-    cellClicked : false, 
-    currentCell : {}, //upon clicked will set currentCell to the cell clicked
-    numberClicked : false, 
-    currentNumber : false,
-    gameOver : false,
-  };
-
-  componentWillMount() {
-    this.createBoard();
-  }
-
-  setUpPlayingBoard = () => {
-    /*This function will set up the game board.
-      If difficulty is set to easy 45 cells of the grid will be revealed. 
-      If difficulty is set to medium 27 cells of the grid will be revealed. 
-      If difficulty is set to hard 18  cells of the grid will be revealed 
-    */
-    console.log(this.state.grid);
-    let revealing = 0;
-    switch (this.state.difficultyIndex) {
-      case 0:
-        revealing = 45;
-        break;
-      case 1:
-        revealing = 27;
-        break;
-      case 2:
-        revealing = 18;
-        break;
-      default:
-        console.log(
-          "shouldn't  have a index over two",
-          this.state.difficultyIndex
-        );
-    }
-    const revealingIndexs = [];
-    while (revealingIndexs.length < revealing) {
-      const j = Math.ceil(Math.random() * 81);
-      if (revealingIndexs.includes(j) === false) {
-        revealingIndexs.push(j);
-      }
-    }
-    const board = [];
-    for (let i = 0; i < this.state.grid.length; i++) {
-      board[i] = this.state.grid[i].slice();
-    }
-    const backTracker = {
+    backTracker :{/*Purpose is to locate the exact spot of the grid. Purpose of backTracker 
+      is to iterate through the pointers to the grid to make the change. I will also decrement 
+      to redo previous if there is a problem with a current position  */
       1: [0, 0],
       2: [0, 1],
       3: [0, 2],
@@ -159,7 +103,66 @@ class App extends Component {
       79: [8, 6],
       80: [8, 7],
       81: [8, 8]
-    };
+    },
+    colorsIndex: 0,
+    grid: [],
+    board: [],
+    group1: [], 
+    group2: [], 
+    group3: [],
+    group4: [], 
+    group5: [],
+    group6: [], 
+    group7: [],
+    group8: [], 
+    group9: [], 
+    cellClicked : false, 
+    currentCell : {}, //upon clicked will set currentCell to the cell clicked
+    numberClicked : false, 
+    currentNumber : false,
+    gameOver : false,
+  };
+
+  componentWillMount() {
+    this.createBoard();
+  }
+
+  setUpPlayingBoard = () => {
+    /*This function will set up the game board.
+      If difficulty is set to easy 45 cells of the grid will be revealed. 
+      If difficulty is set to medium 27 cells of the grid will be revealed. 
+      If difficulty is set to hard 18  cells of the grid will be revealed 
+    */
+    console.log(this.state.grid);
+    let revealing = 0;
+    switch (this.state.difficultyIndex) {
+      case 0:
+        revealing = 45;
+        break;
+      case 1:
+        revealing = 27;
+        break;
+      case 2:
+        revealing = 18;
+        break;
+      default:
+        console.log(
+          "shouldn't  have a index over two",
+          this.state.difficultyIndex
+        );
+    }
+    const revealingIndexs = [];
+    while (revealingIndexs.length < revealing) {
+      const j = Math.ceil(Math.random() * 81);
+      if (revealingIndexs.includes(j) === false) {
+        revealingIndexs.push(j);
+      }
+    }
+    const board = [];
+    for (let i = 0; i < this.state.grid.length; i++) {
+      board[i] = this.state.grid[i].slice();
+    }
+    // removed back tracker so make backTracker equal this.state.backTracker
     console.log(revealingIndexs);
     const indexToGroup ={ 1: "group1",2: "group1",3: "group1",4: "group2",5: "group2", 6: "group2", 7: "group3",8: "group3",9: "group3",10: "group1",11: "group1",12: "group1",13: "group2",14: "group2",15: "group2",16: "group3",17: "group3",18: "group3",19: "group1",20: "group1",21: "group1",22: "group2",23: "group2",24: "group2",25: "group3",26: "group3",27: "group3",28: "group4",29: "group4",30: "group4",31: "group5",32: "group5",33: "group5",34: "group6",35: "group6",36: "group6",37: "group4",38: "group4",39: "group4",40: "group5",41: "group5",42: "group5",43: "group6",44: "group6",45: "group6",46: "group4",47: "group4",48: "group4",49: "group5",50: "group5",51: "group5",52: "group6",53: "group6",54: "group6",55: "group7",56: "group7",57: "group7",58: "group8",59: "group8",60: "group8",61: "group9",62: "group9",63: "group9",64: "group7",65: "group7",66: "group7",67: "group8",68: "group8",69: "group8",70: "group9",71: "group9",72: "group9",73: "group7",74: "group7",75: "group7",76: "group8",77: "group8",78: "group8",79: "group9",80: "group9",81: "group9",};
     const indexToColumn = {1: "column1",2: "column2",3: "column3",4: "column4",5: "column5",6: "column6",7: "column7",8: "column8",9: "column9",10: "column1",11: "column2",12: "column3",13: "column4",14: "column5",15: "column6",16: "column7",17: "column8",18: "column9",19: "column1",20: "column2",21: "column3",22: "column4",23: "column5",24: "column6",25: "column7",26: "column8",27: "column9",28: "column1",29: "column2",30: "column3",31: "column4",32: "column5",33: "column6",34: "column7",35: "column8",36: "column9",37: "column1",38: "column2",39: "column3",40: "column4",41: "column5",42: "column6",43: "column7",44: "column8",45: "column9",46: "column1",47: "column2",48: "column3",49: "column4",50: "column5",51: "column6",52: "column7",53: "column8",54: "column9",55: "column1",56: "column2",57: "column3",58: "column4",59: "column5",60: "column6",61: "column7",62: "column8",63: "column9",64: "column1",65: "column2",66: "column3",67: "column4",68: "column5",69: "column6",70: "column7",71: "column8",72: "column9",73: "column1",74: "column2",75: "column3",76: "column4",77: "column5",78: "column6",79: "column7",80: "column8",81: "column9",}; 
@@ -167,8 +170,8 @@ class App extends Component {
     const boardArray = []; 
     while (startIndex < 82) {
       const tempObj = {};
-      const row = backTracker[startIndex][0];
-      const col = backTracker[startIndex][1];
+      const row = this.state.backTracker[startIndex][0];
+      const col = this.state.backTracker[startIndex][1];
       if (revealingIndexs.includes(startIndex)) {
         tempObj.changable = false;
         tempObj.value = board[row][col];
@@ -326,15 +329,16 @@ class App extends Component {
     return array;
   };
 
-  valueNeeded = (section, toBeAdded) => {
+  valueNeeded = (section, toBeAdded, from) => {
     /*Checks to see if value is needed in section
          section should be an array and toBeAdded should be an integer
          returns true or false performs a check for if section is an array. */
+    console.log(section); 
     let valid = false;
     Array.isArray(section)
       ? (valid = true)
       : console.error(
-          "TypeError : Unexpected Type  section must be an array line 46"
+          `TypeError : Unexpected Type  section must be an array line ${from}`
         );
 
     if (section.includes(toBeAdded)) {
@@ -425,89 +429,7 @@ class App extends Component {
       [0, 0, 0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0, 0]
     ];
-    const backTracker = {
-      1: [0, 0],
-      2: [0, 1],
-      3: [0, 2],
-      4: [0, 3],
-      5: [0, 4],
-      6: [0, 5],
-      7: [0, 6],
-      8: [0, 7],
-      9: [0, 8],
-      10: [1, 0],
-      11: [1, 1],
-      12: [1, 2],
-      13: [1, 3],
-      14: [1, 4],
-      15: [1, 5],
-      16: [1, 6],
-      17: [1, 7],
-      18: [1, 8],
-      19: [2, 0],
-      20: [2, 1],
-      21: [2, 2],
-      22: [2, 3],
-      23: [2, 4],
-      24: [2, 5],
-      25: [2, 6],
-      26: [2, 7],
-      27: [2, 8],
-      28: [3, 0],
-      29: [3, 1],
-      30: [3, 2],
-      31: [3, 3],
-      32: [3, 4],
-      33: [3, 5],
-      34: [3, 6],
-      35: [3, 7],
-      36: [3, 8],
-      37: [4, 0],
-      38: [4, 1],
-      39: [4, 2],
-      40: [4, 3],
-      41: [4, 4],
-      42: [4, 5],
-      43: [4, 6],
-      44: [4, 7],
-      45: [4, 8],
-      46: [5, 0],
-      47: [5, 1],
-      48: [5, 2],
-      49: [5, 3],
-      50: [5, 4],
-      51: [5, 5],
-      52: [5, 6],
-      53: [5, 7],
-      54: [5, 8],
-      55: [6, 0],
-      56: [6, 1],
-      57: [6, 2],
-      58: [6, 3],
-      59: [6, 4],
-      60: [6, 5],
-      61: [6, 6],
-      62: [6, 7],
-      63: [6, 8],
-      64: [7, 0],
-      65: [7, 1],
-      66: [7, 2],
-      67: [7, 3],
-      68: [7, 4],
-      69: [7, 5],
-      70: [7, 6],
-      71: [7, 7],
-      72: [7, 8],
-      73: [8, 0],
-      74: [8, 1],
-      75: [8, 2],
-      76: [8, 3],
-      77: [8, 4],
-      78: [8, 5],
-      79: [8, 6],
-      80: [8, 7],
-      81: [8, 8]
-    }; /*Purpose is to locate the exact spot of the grid. Purpose of backTracker is to iterate through the pointers to the grid to make the change. I will also decrement to redo previous if there is a problem with a current position  */
+    
 
     const maxInput = 405;
 
@@ -535,8 +457,11 @@ class App extends Component {
         grid[7],
         grid[8],
         grid[9]
-      ) !== maxInput ||
-      backTracker > 81
+      ) !== maxInput 
+      // ||
+      //this.state.backTracker > 81 //This line was done on accdient but doesn't make sense
+      //when i put it to the correct && backTrackerIndex > 81 it doesn't work need to research this.
+      //however right now doesn't seem to be needed.  
     ) {
       /*There are three neccessary areas to check
       horizontal sections  which go east and west, or side to side. 
@@ -1005,15 +930,16 @@ class App extends Component {
 
       /*maxInput is where each column is 45 * 9 */
       let attempts = 0; // start attempts at 0 each time.
-      const value = numbers.slice(numbersIndex, numbersIndex + 1);
+      const value = numbers.slice(numbersIndex, numbersIndex + 1); //Value is an array of 1 elements.
+      console.log(backTrackerIndex);
       const doesSectionNeedValue =
-        this.valueNeeded(indexToGroup[backTrackerIndex], value[0]) &&
-        this.valueNeeded(indexToColumn[backTrackerIndex], value[0]) &&
-        this.valueNeeded(indexToRow[backTrackerIndex], value[0]);
+        this.valueNeeded(indexToGroup[backTrackerIndex], value[0], 932) &&
+        this.valueNeeded(indexToColumn[backTrackerIndex], value[0], 933) &&
+        this.valueNeeded(indexToRow[backTrackerIndex], value[0],934);
       /*a && b && c  will return true only if all are true if even one is false it will return false. */
       if (doesSectionNeedValue) {
-        const row = backTracker[backTrackerIndex][0]; //get the row from backTracker.
-        const col = backTracker[backTrackerIndex][1]; // get the col from backTracker.
+        const row = this.state.backTracker[backTrackerIndex][0]; //get the row from backTracker.
+        const col = this.state.backTracker[backTrackerIndex][1]; // get the col from backTracker.
         grid[row][col] = value[0]; // set the section of the grid using the row and col  to the value  which is needed.
         numbersIndex = (numbersIndex + 1) % numbers.length; //number set so go to next number. For the next cell.
         backTrackerIndex += 1; // move forward in the indexing.
@@ -1024,12 +950,12 @@ class App extends Component {
 
           const value = numbers.slice(numbersIndex, numbersIndex + 1);
           const doesSectionNeedValue =
-            this.valueNeeded(indexToGroup[backTrackerIndex], value[0]) &&
-            this.valueNeeded(indexToColumn[backTrackerIndex], value[0]) &&
-            this.valueNeeded(indexToRow[backTrackerIndex], value[0]);
+            this.valueNeeded(indexToGroup[backTrackerIndex], value[0], 949) &&
+            this.valueNeeded(indexToColumn[backTrackerIndex], value[0], 950) &&
+            this.valueNeeded(indexToRow[backTrackerIndex], value[0],951);
           if (doesSectionNeedValue) {
-            const row = backTracker[backTrackerIndex][0]; //get the row from backTracker.
-            const col = backTracker[backTrackerIndex][1]; // get the col from backTracker.
+            const row = this.state.backTracker[backTrackerIndex][0]; //get the row from backTracker.
+            const col = this.state.backTracker[backTrackerIndex][1]; // get the col from backTracker.
             grid[row][col] = value[0]; // set the section of the grid using the row and col  to the value  which is needed.
             numbersIndex = (numbersIndex + 1) % numbers.length; //number set so go to next number. For the next cell.
             backTrackerIndex += 1; // move forward in the indexing.
@@ -1142,7 +1068,8 @@ class App extends Component {
       If it is a repeat,  repeated is changed to true and that cell value will be red to indicate 
       it is not a valid choice. 
     */
-    console.log("updateCell checking")
+
+    
    
   }
 
