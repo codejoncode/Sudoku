a44f958cbb0068c4a13865a41679270fd5fa13e4
codejoncode <jonathanjholloway@gmail.com>
2019-01-20 00:07:16 -0600

    Updates

---
+++ precache-manifest.f81e76addab25122839e2dac50bd6467.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f18813b2aff252d7cb99",
+    "url": "/Sudoku/static/js/runtime~main.f18813b2.js"
+  },
+  {
+    "revision": "736819ea92123681405e",
+    "url": "/Sudoku/static/js/main.736819ea.chunk.js"
+  },
+  {
+    "revision": "7f50a2bc032c0b4d9cf1",
+    "url": "/Sudoku/static/js/1.7f50a2bc.chunk.js"
+  },
+  {
+    "revision": "736819ea92123681405e",
+    "url": "/Sudoku/static/css/main.55b0f875.chunk.css"
+  },
+  {
+    "revision": "6db6fa236869113706018ef3630391e1",
+    "url": "/Sudoku/index.html"
+  }
+];
