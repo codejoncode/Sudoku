938cda1ca7ff1276c536cc088b7d3a895484434b
codejoncode <jonathanjholloway@gmail.com>
2019-04-21 21:11:46 -0500

    added color only going with two colors instead of a combination of 9

--- client/src/Components/Grid.js
+++ client/src/Components/Grid.js
@@ -5,11 +5,13 @@ class Grid extends Component {
   state = {};
 
   render() {
+    const color1 = "#B0CCE8";
+    const color2 = "#57A4BC";
     const {onCellHoverOut, onCellHover, onCellClick, group1, group2, group3, group4, group5, group6, group7, group8, group9} = this.props;
     const groups = [group1, group2, group3, group4, group5, group6, group7, group8, group9]; 
     return (
       <div className="grid-container">
-        {groups.map((group, id) => <ThreeByThree  key = {id} onCellHoverOut = {onCellHoverOut} onCellHover = {onCellHover} onCellClick = {onCellClick} group = {group}/>)}
+        {groups.map((group, id) => <ThreeByThree  key = {id} onCellHoverOut = {onCellHoverOut} onCellHover = {onCellHover} onCellClick = {onCellClick} group = {group} color = {id % 2 === 0 ? color1 : color2}/>)}
         {/* remvoed color   color="#B0CCE8" color="#0094B7" color="#57A4BC" color="#0093D4" color="#D0E3F1" color="#8CAAD2" color="#4EADDD" color="#84BEE5" color="#8CAAD2" color="#84C4CB" */}
       </div>
     );
